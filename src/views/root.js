109644b5627fff3d9c7d53b698354175d7ca7a3e
mistakia <1823355+mistakia@users.noreply.github.com>
2022-03-21 14:30:36 -0400

    chore: cleanup

--- src/views/root.js
+++ src/views/root.js
@@ -1,33 +1,15 @@
 import React from 'react'
-import { create } from 'jss'
 import { Provider } from 'react-redux'
 import { HistoryRouter as Router } from 'redux-first-history/rr6'
-import {
-  createTheme,
-  createGenerateClassName,
-  StylesProvider,
-  ThemeProvider,
-  jssPreset
-} from '@material-ui/core/styles'
 
 import { store, history } from '@core/store.js'
 import App from '@components/app/index.js'
 
-const theme = createTheme()
-const jss = create({ plugins: [...jssPreset().plugins] })
-const generateClassName = createGenerateClassName({
-  productionPrefix: navigator.userAgent === 'ReactSnap' ? 'snap' : 'jss'
-})
-
 const Root = () => (
   <Provider store={store}>
-    <StylesProvider jss={jss} generateClassName={generateClassName}>
-      <ThemeProvider theme={theme}>
-        <Router history={history}>
-          <App />
-        </Router>
-      </ThemeProvider>
-    </StylesProvider>
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>
 )
 
