1650a010d41cf606424a7486da356e21a1f84284
mistakia <1823355+mistakia@users.noreply.github.com>
2024-01-04 11:47:56 -0500

    feat: cache coingecko api response

--- libs-shared/coingecko.mjs
+++ libs-shared/coingecko.mjs
@@ -1,10 +1,17 @@
 import fetch from 'node-fetch'
 
+let coins_cache = null
+
 export const getCoins = async () => {
   const url = 'https://api.coingecko.com/api/v3/coins/list'
   const res = await fetch(url)
   const data = await res.json()
-  return data
+
+  if (data && data.length) {
+    coins_cache = data
+  }
+
+  return coins_cache || []
 }
 
 export const getCoin = async ({ symbol }) => {
