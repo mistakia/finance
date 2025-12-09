3057c92ca654bb98d431f14005cba119203d925d
mistakia <1823355+mistakia@users.noreply.github.com>
2025-12-09 12:39:24 -0500

    chore: update development server ports in constants for API and WebSocket connections

--- src/core/constants.js
+++ src/core/constants.js
@@ -3,9 +3,9 @@
 //  GENERAL
 // -------------------------------------
 export const BASE_URL = IS_DEV
-  ? 'http://192.168.1.113:8080'
+  ? 'http://192.168.1.113:8084'
   : 'https://tint.finance'
 export const API_URL = `${BASE_URL}/api`
 export const WEBSOCKET_URL = IS_DEV
-  ? 'ws://192.168.1.113:8080'
+  ? 'ws://192.168.1.113:8084'
   : 'wss://tint.finance'
